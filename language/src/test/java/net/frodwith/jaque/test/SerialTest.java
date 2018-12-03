efa8bd7599939028a454d82d75b515a60369c29d
Paul Driver <frodwith@gmail.com>
2018-12-03 15:20:54 -0800

    add some random testing to the serialization test

--- language/src/test/java/net/frodwith/jaque/test/SerialTest.java
+++ language/src/test/java/net/frodwith/jaque/test/SerialTest.java
@@ -1,8 +1,11 @@
 package net.frodwith.jaque.test;
 
+import java.util.Random;
+
 import org.junit.Test;
 
 import net.frodwith.jaque.data.Cell;
+import net.frodwith.jaque.runtime.Atom;
 import net.frodwith.jaque.runtime.Equality;
 import net.frodwith.jaque.runtime.HoonSerial;
 import net.frodwith.jaque.runtime.Cords;
@@ -20,6 +23,25 @@ public class SerialTest {
     return Cords.fromString(s);
   }
 
+  private static Object randomAtom(Random r) {
+    byte[] bytes = new byte[r.nextInt(8)];
+    r.nextBytes(bytes);
+    return Atom.fromByteArray(bytes);
+  }
+
+  private static Cell randomCell(Random r, int depth) {
+    return new Cell(randomNoun(r, depth+1), randomNoun(r, depth+1));
+  }
+
+  private static Object randomNoun(Random r, int depth) {
+    if ( depth < 10 && r.nextBoolean() ) {
+      return randomCell(r, depth);
+    }
+    else {
+      return randomAtom(r);
+    }
+  }
+
   @Test
   public void testExamples() throws ExitException {
     Object a = 5456L, b = 42L;
@@ -29,5 +51,15 @@ public class SerialTest {
     b = new Cell(c("foo"), c("bar"));
     aeq("jam 2", a, HoonSerial.jam(b));
     aeq("cue 2", b, HoonSerial.cue(a));
+  }
+
+  // QuickCheck would be better, but we can make do
+  @Test
+  public void testRandom() throws ExitException {
+    Random r = new Random();
+    for ( int i = 0; i < 100; ++i ) {
+      Object noun = randomNoun(r, 0);
+      aeq("random jam #" + i, noun, HoonSerial.cue(HoonSerial.jam(noun)));
+    }
   }
 }
