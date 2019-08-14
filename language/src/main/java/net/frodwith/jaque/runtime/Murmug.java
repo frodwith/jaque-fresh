8b398c5b922c7e604914800d9de334756dd4df04
Joe Bryan <joemfb@gmail.com>
2019-08-13 17:49:20 -0700

    adds murmur32 mug implementation and tests

---
+++ language/src/main/java/net/frodwith/jaque/runtime/Murmug.java
@@ -0,0 +1,61 @@
+package net.frodwith.jaque.runtime;
+
+import de.greenrobot.common.hash.Murmur3A;
+
+import net.frodwith.jaque.data.BigAtom;
+import net.frodwith.jaque.data.Cell;
+import net.frodwith.jaque.runtime.Atom;
+
+public final class Murmug {
+  public static int get(Cell c) {
+    return calculate(c);
+  }
+
+  public static int get(BigAtom a) {
+    return calculate(a);
+  }
+
+  public static int get(long l) {
+    return bytes(Atom.toByteArray(l));
+  }
+
+  public static int get(Object noun) {
+    return ( noun instanceof Cell )
+      ? get((Cell) noun)
+      : ( noun instanceof BigAtom )
+      ? get((BigAtom) noun)
+      : get((long) noun);
+  }
+
+  public static int calculate(Cell c) {
+    return both(get(c.head), get(c.tail));
+  }
+
+  public static int calculate(BigAtom a) {
+    return bytes(a.asByteArray());
+  }
+
+  public static int both(int a, int b) {
+    return get((long)(a ^ (0x7fffffff ^ b)));
+  }
+
+  public static int bytes(byte[] b) {
+    Murmur3A murmur;
+    int  seed = 0xcafebabe;
+    long hash = 0L;
+
+    while ( true ) {
+      murmur = new Murmur3A(seed);
+      murmur.update(b);
+      hash = murmur.getValue();
+      hash = (hash >>> 31) ^ (hash & 0x7fffffffL);
+
+      if ( 0L == hash ) {
+        ++seed;
+      }
+      else {
+        return (int)hash;
+      }
+    }
+  }
+}
---
+++ language/src/test/java/net/frodwith/jaque/test/MurmugTest.java
@@ -0,0 +1,69 @@
+package net.frodwith.jaque.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import net.frodwith.jaque.data.Cell;
+import net.frodwith.jaque.data.BigAtom;
+import net.frodwith.jaque.runtime.Atom;
+import net.frodwith.jaque.runtime.HoonMath;
+import net.frodwith.jaque.runtime.Murmug;
+
+public class MurmugTest {
+
+  //  XX put this somewhere
+  //
+  public Object fromString(String s) {
+    try {
+      return Atom.fromByteArray(s.getBytes("UTF-8"));
+    }
+    catch (java.io.UnsupportedEncodingException e) {
+      return 0L;
+    }
+  }
+
+  @Test
+  public void testStrings() {
+    assertEquals(0x4d441035, Murmug.get(fromString("Hello, world!")));
+    assertEquals(0x64dfda5c,
+      Murmug.get(fromString("xxxxxxxxxxxxxxxxxxxxxxxxxxxx")));
+  }
+
+  @Test
+  public void testSmallAtom() {
+    assertEquals(0x643849c6, Murmug.get(42L));
+    assertEquals(0x79ff04e8, Murmug.get(0L));
+  }
+
+  @Test
+  public void testBigAtom() {
+    BigAtom a = new BigAtom(new int[] { 0xdeadbeef, 0xbeefdead, 0xfeedbeef });
+    assertEquals(0x601265fc, Murmug.get(a));
+
+    a = (BigAtom)HoonMath.lsh((byte)3, 1,
+      HoonMath.add(new BigAtom((int[])HoonMath.bex(212L)),
+                   fromString("abcdefjhijklmnopqrstuvwxyz"))
+    );
+    assertEquals(0x34d08717, Murmug.get(a));
+  }
+
+  @Test
+  public void testCell() {
+    Cell a = new Cell(42L, 0L);
+    assertEquals(0x5303a809, Murmug.get(a));
+
+    a = new Cell(0L, 0L);
+    assertEquals(0x389ca03a, Murmug.get(a));
+
+    a = new Cell(1L, 1L);
+    assertEquals(0x389ca03a, Murmug.get(a));
+
+    a = new Cell(0L, HoonMath.bex(32L));
+    assertEquals(0x5258a6c0, Murmug.get(a));
+
+    a = new Cell(HoonMath.dec(new BigAtom((int[])HoonMath.bex(128L))), 1L);
+    assertEquals(0x2ad39968, Murmug.get(a));
+  }
+}
