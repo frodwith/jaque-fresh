15795f1f79b496889565024f78c783564c749986
Elliot Glaysher <elliot@tlon.io>
2019-10-24 14:56:05 -0700

    Skeleton of separate Serf binary on the side.

---
+++ serf/src/main/java/net/frodwith/Serf.java
@@ -0,0 +1,15 @@
+package net.frodwith;
+
+import net.frodwith.jaque.NockLanguage;
+
+/**
+ * Hello world!
+ *
+ */
+public class Serf
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello mime type, " + NockLanguage.MIME_TYPE );
+    }
+}
